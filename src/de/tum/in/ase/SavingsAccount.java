f8f4433d2f420ac3e0de62a618f97a85ef70104c
Stephan Krusche <krusche@tum.de>
2022-11-22 08:48:34 +0100

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/in/ase/SavingsAccount.java
@@ -0,0 +1,16 @@
+package de.tum.in.ase;
+
+public class SavingsAccount extends BankAccount {
+    protected final double interestRate;
+
+    public SavingsAccount(int accountId, double initial, double rate) {
+        super(accountId, initial);
+        interestRate = rate;
+    }
+
+    public void addInterest() {
+        balance = balance * (1 + interestRate);
+        System.out.println("Interest added to account: " + accountId
+                + "\nNew balance:\t" + balance + "\n");
+    }
+}
