e5f67016244b21215a77023dc92001d0860d8699
ge32goy <hanyin.chen@tum.de>
2022-11-23 16:08:17 +0100

    commit

---
+++ src/de/tum/in/ase/CreditCardAccount.java
@@ -0,0 +1,22 @@
+package de.tum.in.ase;
+
+public class CreditCardAccount extends BankAccount {
+    private double creditBalance = 0;
+    private final double limit;
+
+    public CreditCardAccount(int accountId, double initial, double limit) {
+        super(accountId, initial);
+        this.limit = limit;
+    }
+
+    public boolean pay(double amount) {
+        System.out.println("Pay from account " + accountId + "\n" + "Amount:\t\t\t" + amount);
+        if (- creditBalance + amount > limit) {
+            System.out.println("Sorry, insufficient funds...\n");
+            return false;
+        }
+        creditBalance = creditBalance - amount;
+        System.out.println("New balance:\t" + creditBalance + "\n");
+        return true;
+    }
+}
